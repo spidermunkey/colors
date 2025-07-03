973bb1d427eb9f6296f3a7efc72b6061716faef6
spider <burkstremane@gmail.com>
2025-07-03 12:20:58 -0600

    update editor html

--- latest/src/Editor.jsx
+++ latest/src/Editor.jsx
@@ -1,13 +1,29 @@
 export const Editor = () => {
   return (
     <div className="editor-modal">
-      <div className="editor">
-        <div className="modal-header"></div>
-        <div className="modal-content"></div>
-      </div>
-      <div className="browser">
+      <div className="modal-wrapper">
+        <div className="color-card">
+          <div className="card-info info">
+            <div className="name-label info-label editor-component" component='name'>Name</div>
+            <div className="hex-label info-label editor-component" component='hex'>hex</div>
+          </div>
+          <div className="card-editor editor">
+            <div className="editor-wheel"></div>
+            <div className="editor-sliders">
+              
+            </div>
+            <div className="editor-values">
+              <div className="hue-label value-label editor-component" component='hue'>Hue</div>
+            <div className="sat-label value-label editor-component" component='sat'>Saturation</div>
+            <div className="light-label value-label editor-component" component='light'>Light</div>
+            </div>
+          </div>
+        </div>
 
+        <div className="color-preview"></div>
       </div>
+
+      <div className="modal-options"></div>
     </div>
   )   
 }
